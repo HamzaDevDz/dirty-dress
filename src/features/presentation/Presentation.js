39d8d4d18bf6d316e913278cdbe2ebdf87a20014
Baghdad <BaghdadHamdoud@gmail.com>
2021-11-07 21:30:08 +0100

    presentation

---
+++ src/features/presentation/Presentation.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './Presenttion.css'
+
+export const Presentation = () => {
+    return(
+        <div className={'presentation'}>
+            <div className={'presentation__img'}>
+                <img src={'hot1.png'} alt={''}/>
+            </div>
+            <div className={'presentation__data'}>
+                <h1>Dirty Dress</h1>
+                <h2>Wiering as a Godess, and why not, be the dominatrice of the world.</h2>
+                <button className={'presentation__data__btn'}>Check Out</button>
+            </div>
+        </div>
+    )
+}
